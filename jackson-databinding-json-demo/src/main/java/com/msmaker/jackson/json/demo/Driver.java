b8010514d11570e6cf0562cfc402cb76825f51c4
Marco Silva <marcosilva.tec@gamil.com>
2021-06-12 17:27:36 -0300

    Json test

---
+++ jackson-databinding-json-demo/src/main/java/com/msmaker/jackson/json/demo/Driver.java
@@ -0,0 +1,42 @@
+package com.msmaker.jackson.json.demo;
+
+import java.io.File;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Driver {
+
+	public static void main(String[] args) {
+
+		try {
+
+			// create object mapper
+			ObjectMapper mapper = new ObjectMapper();
+
+			// read JSON file and map/convert to Java Pojo:
+			// data/sample-lite.json
+
+			Student theStudent = mapper.readValue(new File("data/sample-full.json"), Student.class);
+
+			// print first name and last name
+			System.out.println("First Name = " + theStudent.getFirstName());
+			System.out.println("Last Name = " + theStudent.getLastName());
+
+			// print out address: street and city
+			Address tempAdreess = theStudent.getAddress();
+
+			System.out.println("Street = " + tempAdreess.getStreet());
+			System.out.println("City = " + tempAdreess.getCity());
+
+			// print out languages
+
+			for (String tempLang : theStudent.getLanguages()) {
+				System.out.println(tempLang);
+			}
+
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		}
+	}
+
+}
